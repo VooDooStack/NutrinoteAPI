a6adc86b3e875e98dcc262452f9a601a1bf36b4e
Colton Bristow <coltonbristow@Coltons-MBP.attlocal.net>
2023-02-24 13:51:38 -0600

    trying different connection string.

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -11,6 +11,7 @@ public static class ApplicationServiceExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
+        // var connectionString = $"database={Environment.GetEnvironmentVariable("database")};server={Environment.GetEnvironmentVariable("server")};port=5432;uid={Environment.GetEnvironmentVariable("user")};pwd={Environment.GetEnvironmentVariable("user_pass")};MinPoolSize=0;MaxPoolSize=100;";
         //services.AddSingleton<FirebaseApp>(FirebaseApp.Create());
         services.AddControllers((options) =>
         {
@@ -21,8 +22,7 @@ public static class ApplicationServiceExtensions
         services.AddSwaggerGen();
         services.AddDbContext<Persistence.DataContext>(opt =>
         {
-            opt.UseNpgsql(
-                $"database={Environment.GetEnvironmentVariable("database")};server={Environment.GetEnvironmentVariable("server")};port=5432;uid={Environment.GetEnvironmentVariable("user")};pwd={Environment.GetEnvironmentVariable("user_pass")};MinPoolSize=0;MaxPoolSize=100;");
+            opt.UseNpgsql(config.GetConnectionString("DefaultConnectionString"));
         });
         services.AddMediatR(typeof(List.Handler));
         services.AddAutoMapper(typeof(MappingProfiles).Assembly);
